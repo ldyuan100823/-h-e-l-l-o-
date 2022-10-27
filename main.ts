0bde5c5f67c57582e109c84579db83efd96f3a56
ldyuan100823 <116774658+ldyuan100823@users.noreply.github.com>
2022-10-27 16:02:39 +1400

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
 basic.forever(function () {
+    if (input.isGesture(Gesture.Shake)) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+    }
+})
+basic.forever(function () {
     if (input.lightLevel() == 80) {
         pins.servoWritePin(AnalogPin.P2, 180)
     } else {
